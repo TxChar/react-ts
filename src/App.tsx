3873d29890d3c1d8fa3b035d9cba0043ee506922
TxChar <pontogreen1@gmail.com>
2025-02-20 23:24:01 +0700

    refactor: 💡 Using Routes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// src/App.tsx
+// import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Button from './components/Button'
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
 
   return (
-
-    <div className="min-h-screen bg-gray-100">
-            <Navbar />
-    <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
-      <div className="flex gap-4 mb-8">
-        <a  href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo react w-20 h-20" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react react w-20 h-20" alt="React logo" />
-        </a>
-      </div>
-      <div className="text-center">
-        <div className="mb-6">
-        < Button onClick={() => setCount((count) => count + 1)} count={count}/>
-        </div>
-
-        <p className="mb-2">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-    </div>
-
-  )
+  <div className="min-h-screen">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  </div>
+  );
 }
 
-export default App
-
-// function App() {
-//   return (
-//     <h1 className="text-4xl text-blue-500">Vite Project</h1>
-//   )
-// }
-
-// export default App;
+export default App;
