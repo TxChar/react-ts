614793f87e6bc810f6f5e26e5dcd9ff76d81c571
TxChar <pontogreen1@gmail.com>
2025-02-22 14:37:01 +0700

    feat: 🎸 Create banner and banner slider

---
+++ src/components/BannerSlider.tsx
@@ -0,0 +1,69 @@
+// src/components/BannerSlider.tsx
+// import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import ihavecpu1 from '../assets/ihavecpu1.jpg';
+import ihavecpu2 from '../assets/ihavecpu2.jpg';
+import ihavecpu3 from '../assets/ihavecpu3.jpg';
+
+const images = [
+    ihavecpu1,
+    ihavecpu2,
+    ihavecpu3,
+];
+
+// const BannerSlider: React.FC = () => {
+const BannerSlider = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        customPaging: () => (
+            <div className="w-4 h-4 grid justify-items-center bg-gray-400 rounded-full hover:bg-gray-600"></div>
+        ),
+        appendDots: (dots: React.ReactNode) => (
+            <div style={{
+                position: 'absolute',
+                top: '50%',            /* จัดให้อยู่ตรงกลางแนวตั้ง */
+                left: '50%',           /* จัดให้อยู่ตรงกลางแนวนอน */
+                transform: 'translate(-50%, 20%)', /* เลื่อนให้จุดอยู่ตรงกลางพอดี */
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <ul style={{ display: 'flex', gap: '10px', padding: 0, listStyle: 'none' }}>
+                    {dots}
+                </ul>
+            </div>
+        )
+    };
+
+    return (
+        <div className="w-full max-w-7xl mx-auto">
+            <div className="block p-2 bg-white border border-gray-200 rounded-lg shadow-sm">
+
+            <Slider {...settings}>
+                {images.map((src, index) => (
+                    <div key={index} className="relative w-full h-[400px]">
+                        <img
+                            src={src}
+                            alt={`banner-${index}`}
+                            className="w-full h-full object-cover rounded-lg"
+                            loading="lazy"
+                            />
+                    </div>
+                ))}
+            </Slider>
+            </div>
+        </div>
+    );
+};
+
+export default BannerSlider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,24 @@
 // src/pages/Home.tsx
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
-import Button from '../components/Button';
-import { useState } from 'react';
-const Home = () => {
-    const [count, setCount] = useState(0);
-    return (
-        <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white mt-[64px]">
-        <div className="flex gap-4 mb-8">
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo react w-20 h-20" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react w-20 h-20" alt="React logo" />
-          </a>
-        </div>
+import {Banner} from '../components/Banner';
+import BannerSlider from '../components/BannerSlider';
+// import PartnerSlider from '../components/PartnerSlider';
 
+const Home = () => {
+    return (
+      <div className="min-h-screen flex flex-col items-center text-white mt-[72px]">
+        <BannerSlider />
+        <div className="grid grid-cols-3 gap-16 mt-[18px]">
+          <Banner />
+          <Banner />
+          <Banner />
+        </div>
+        {/* <PartnerSlider /> */}
+        <div>
+        </div>
         <div className="text-center">
           <div className="mb-6">
-            <Button onClick={() => setCount((count) => count + 1)} count={count} />
           </div>
-
-          <p className="mb-2">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
         </div>
-
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
     );
   };
